caa0bfe7110306ef91281af873ae108b1341827f
Maximilian Caspar <maximilian.caspar@web.de>
2016-06-17 13:32:39 +0200

    Another include fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,18 @@
 #include <iostream>
 #include "gravity.h"
 
+#define _G 6.674E-11;
+#define _MASS_OF_SUN 1.98892E30;
+#define _LIGTHYEAR 9.46073E15;
+
 int main(int argc, char const *argv[])
 {
 	std::vector<Star> elements;
-	make_galaxy(elements, 9.461E20, 1000, 0, 0, 0, 0);
-	int i = 0;
-	for(double t = 0; t < 864000000; t+= 86400000)
+	make_galaxy(elements, 9.461E20, 5000, 0, 0, 0, 0);
+	for(int t = 1; t < 100; t++)
 	{
-		i++;
-		std::cout << i << " " << t << " " << elements[2].xpos << " " << elements[2].xfor<<"\n";
-		accelerate(elements, 86400000);
+		std::cout << t << " "  << elements[2].xpos << " " << elements[2].xvel<<"\n";
+		accelerate(elements, 8E15);
 
 	}
 	return 0;
